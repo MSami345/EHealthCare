42a262099fafea915f8a96fcdef79f96756c03f0
Muhammad Samiullah <73605710+MSami345@users.noreply.github.com>
2023-10-21 18:59:30 +0500

    Change in App.js

--- App.js
+++ App.js
@@ -1,47 +1,6 @@
-{
-  // import { useEffect } from "react";
-  // import MyStack from "./navigation/MyStack";
-  // import AsyncStorage from "@react-native-async-storage/async-storage";
-  // import { useNavigation } from "@react-navigation/native";
-  // export default function App() {
-  //   let screen;
-  //   useEffect(() => {
-  //     getScreen();
-  //   }, [screen]);
-  //   async function getScreen() {
-  //     screen = await AsyncStorage.getItem("screen");
-  //     console.log(screen);
-  //     const navigation = useNavigation();
-  //     navigation.navigate(screen);
-  //   }
-  //   return <MyStack />;
-  // }
-}
-import { useEffect } from "react";
 import MyStack from "./navigation/MyStack";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { NavigationContainer } from "@react-navigation/native";
-import { View } from "react-native-web";
+//I have committed changes in app.js
 
 export default function App() {
-  // useEffect(() => {
-  //   getScreen();
-  // }, []);
-
-  // async function getScreen() {
-  //   const screen = await AsyncStorage.getItem("screen");
-  //   if (screen) {
-  //     // You can access the navigation prop here and navigate to the screen
-  //     navigateToScreen(screen);
-  //   }
-  // }
-
-  // // Define a function to navigate to the specified screen
-  // const navigateToScreen = (screen) => {
-  //   MyStack.navigation.navigate(screen);
-  // };
-
-  return (
-    <MyStack />
-  );
+  return <MyStack />;
 }
